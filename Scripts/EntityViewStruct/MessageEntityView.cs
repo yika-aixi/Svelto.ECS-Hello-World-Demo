5af284c3c7b8bba77454bcc25be18f5cb04d7d84
Icarus <1375400884@qq.com>
2019-09-12 16:19:52 +0800

    !TX 修正

--- Scripts/EntityViewStruct/MessageEntityView.cs
+++ Scripts/EntityViewStruct/MessageEntityView.cs
@@ -1,10 +1,12 @@
 ﻿using Svelto.ECS;
+using Svelto.ECS.Experimental;
 using Svelto.ECS.Hybrid;
 
 namespace DefaultNamespace
 {
-    public class MessageEntityView:IEntityViewStruct
+    public struct MessageEntityView:IEntityViewStruct
     {
+        public ECSString Message;
         public EGID ID { get; set; }
     }
 }
